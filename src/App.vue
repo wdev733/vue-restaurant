8e27f50b8aea139d3132920745deb06e5ceae2ba
wdev733 <wdev733@gmail.com>
2018-12-15 21:57:56 +0100

    add navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <template>
-  <div id="app">
-    <el-main>
-      <div>
-        <router-link to="/">Accueil</router-link>
-        <router-link to="/restaurants">Restaurants</router-link>
-        <router-link to="/hello">Hello World !</router-link>
-      </div>
-      <router-view></router-view>
-    </el-main>
-  </div>
+    <div id="app">
+        <el-main>
+            <Navigation></Navigation>
+            <router-view></router-view>
+        </el-main>
+    </div>
 </template>
 
 <script>
+import Navigation from './containers/Navigation'
+
 export default {
-  name: 'app',
+    name: 'app',
+    components: {
+        Navigation,
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 </style>
---
+++ src/containers/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-menu 
+        router="true"
+        :default-active="activeIndex" 
+        class="el-menu-demo" 
+        mode="horizontal"
+        @select="handleSelect">
+        <el-menu-item index="/">Accueil</el-menu-item>
+        <el-menu-item index="/restaurants">Restaurants</el-menu-item>
+        <el-menu-item index="4">Ajouter un restaurant</el-menu-item>
+        <el-menu-item index="5">Commande</el-menu-item>
+        <el-menu-item index="/hello">Hello World !</el-menu-item>
+    </el-menu>
+</template>
+
+<script>
+export default {
+    methods: {
+        handleSelect() {
+            return
+        }
+    }
+}
+</script>
