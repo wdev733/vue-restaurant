f7732e2ca039035a2adfb957ea31d4356ee3d7f3
wdev733 <wdev733@gmail.com>
2018-12-17 11:55:00 +0100

    InformationsRestaurant insert to modal

---
+++ src/containers/Restaurants/RestaurantModal/InformationsTab.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="informationsRestaurant">
+        <el-row>
+            <img id="imageRestaurant" :src="image"/>
+            <ul>
+                <li><b>Cuisine : </b>{{restaurant.cuisine}}</li>
+                <li v-if="restaurant.address.street">
+                    <b>Adresse : </b>
+                    <ul>
+                        <li><b>Batiment : </b>{{restaurant.address.building}}</li>
+                        <li><b>Rue : </b>{{restaurant.address.street}}</li>
+                        <li><b>Code postale : </b>{{restaurant.address.zipcode}}</li>
+                        <li><b>Quartier : </b>{{restaurant.borough}}</li>
+                    </ul>
+                </li>
+            </ul>
+        </el-row>
+        <MapTool v-if="restaurant.address.street"></MapTool>
+    </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+import MapTool from '../../../components/MapTool.vue';
+
+export default {
+    name: 'RestaurantModal',
+    computed: {
+        ...mapState({
+            image: state => state.restaurants.imageCurrentRestaurant,
+        }),
+        ...mapGetters({
+            restaurant: 'restaurants/restaurantSelected',
+        }),
+    },
+    components: { 
+        MapTool
+    },
+}
+</script>
+
+<style>
+#imageRestaurant {
+    max-width: 400px;
+    max-height: 400px;
+}
+</style>
--- src/containers/Restaurants/RestaurantModal/index.vue
+++ src/containers/Restaurants/RestaurantModal/index.vue
@@ -4,42 +4,26 @@
         :visible.sync="openModal"
         :before-close="toggleOpenModal"
         width="90%">
-            <el-row id="detailsRestaurant">
-                <img id="imageRestaurant" :src="image"/>
-                <ul>
-                    <li><b>Cuisine : </b>{{restaurant.cuisine}}</li>
-                    <li v-if="restaurant.address.street">
-                        <b>Adresse : </b>
-                        <ul>
-                            <li><b>Batiment : </b>{{restaurant.address.building}}</li>
-                            <li><b>Rue : </b>{{restaurant.address.street}}</li>
-                            <li><b>Code postale : </b>{{restaurant.address.zipcode}}</li>
-                            <li><b>Quartier : </b>{{restaurant.borough}}</li>
-                        </ul>
-                    </li>
-                </ul>
-            </el-row>
-            <MapTool v-if="restaurant.address.street">></MapTool>
+        <InformationsTab></InformationsTab>
     </el-dialog>
 </template>
 
 <script>
-import { mapState, mapActions, mapGetters } from 'vuex'
-import MapTool from '../../../components/MapTool.vue';
+import { mapState, mapActions, mapGetters, } from 'vuex'
+
+import InformationsTab from './InformationsTab'
 
 export default {
-    name: 'RestaurantModal',
     computed: {
         ...mapState({
             openModal: state => state.restaurants.modals.openDetailsModal,
-            image: state => state.restaurants.imageCurrentRestaurant,
         }),
         ...mapGetters({
             restaurant: 'restaurants/restaurantSelected',
         }),
     },
-    components: { 
-        MapTool
+    components: {
+        InformationsTab
     },
     methods: {
         ...mapActions({
@@ -48,10 +32,3 @@ export default {
     }
 }
 </script>
-
-<style>
-#imageRestaurant {
-    max-width: 400px;
-    max-height: 400px;
-}
-</style>
