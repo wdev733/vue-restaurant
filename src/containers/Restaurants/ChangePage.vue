16510e8c927a40100cc363d819cc9237f131d8a4
wdev733 <wdev733@gmail.com>
2018-12-22 15:47:01 +0100

    change display number page

--- src/containers/Restaurants/ChangePage.vue
+++ src/containers/Restaurants/ChangePage.vue
@@ -1,70 +1,58 @@
 <template>
-    <div id="changePage">
-        <el-row :span="8">
-            <el-col id="countRestaurant">
-                <label>Nombre de restaurants : {{count}}</label>
-            </el-col>
+  <div id="changePage">
+    <el-row :span="8">
+      <el-col id="countRestaurant">
+        <label>Nombre de restaurants : {{count}}</label>
+      </el-col>
 
-            <el-col id="changePageSize">
-                <label for="changePagination">Nombre de restaurants par page : {{pageSize}}</label>
-                <el-slider
-                    type="range" 
-                    id="changePagination" 
-                    name="changePagination" 
-                    :min="5" :max="100" :value="pageSize" :step="5"
-                    v-on:input="changePageSize" />
-            </el-col>
-        </el-row>
+      <el-col id="changePageSize">
+        <label for="changePagination">Nombre de restaurants par page : {{pageSize}}</label>
+        <el-slider
+          type="range"
+          id="changePagination"
+          name="changePagination"
+          :min="5"
+          :max="100"
+          :value="pageSize"
+          :step="5"
+          v-on:input="changePageSize"
+        />
+      </el-col>
+    </el-row>
 
-        <el-row id="changePage" :span="24">
-            <el-button  round 
-                        native-type="button"
-                        v-on:click="refreshRestaurants">
-                        Refresh
-            </el-button>
-            
-            <el-button  icon="el-icon-d-arrow-left" 
-                        v-on:click="firstPage()"
-                        circle>
-            </el-button>
-            <el-button  icon="el-icon-arrow-left" 
-                        v-on:click="decrementPage()"
-                        circle>
-            </el-button>
-            <p>{{page}}</p>
-            <el-button  icon="el-icon-arrow-right" 
-                        v-on:click="incrementPage()"
-                        circle>
-            </el-button>
-            <el-button  icon="el-icon-d-arrow-right" 
-                        v-on:click="lastPage()"
-                        circle>
-            </el-button>
-        </el-row>
-    </div>
+    <el-row id="changeNumPage" :span="24">
+      <el-button round native-type="button" v-on:click="refreshRestaurants">Refresh</el-button>
+
+      <el-button icon="el-icon-d-arrow-left" v-on:click="firstPage()" circle></el-button>
+      <el-button icon="el-icon-arrow-left" v-on:click="decrementPage()" circle></el-button>
+      <el-tag>{{page}}</el-tag>
+      <el-button icon="el-icon-arrow-right" v-on:click="incrementPage()" circle></el-button>
+      <el-button icon="el-icon-d-arrow-right" v-on:click="lastPage()" circle></el-button>
+    </el-row>
+  </div>
 </template>
 
 <script>
-import { mapState, mapActions, } from 'vuex'
+import { mapState, mapActions } from "vuex";
 
 export default {
-    name: 'ChangePage',
-    computed: {
-        ...mapState({
-            count: state => state.restaurants.count,
-            pageSize: state => state.restaurants.table.pageSize,
-            page: state => state.restaurants.table.page,
-        }),
-    },
-    methods: {
-        ...mapActions({
-            incrementPage: 'restaurants/incrementPage',
-            decrementPage: 'restaurants/decrementPage',
-            firstPage: 'restaurants/firstPage',
-            lastPage: 'restaurants/lastPage',
-            changePageSize: 'restaurants/setPageSize',
-            refreshRestaurants: 'restaurants/refreshRestaurants',
-        }),
-    },
-}
+  name: "ChangePage",
+  computed: {
+    ...mapState({
+      count: state => state.restaurants.count,
+      pageSize: state => state.restaurants.table.pageSize,
+      page: state => state.restaurants.table.page
+    })
+  },
+  methods: {
+    ...mapActions({
+      incrementPage: "restaurants/incrementPage",
+      decrementPage: "restaurants/decrementPage",
+      firstPage: "restaurants/firstPage",
+      lastPage: "restaurants/lastPage",
+      changePageSize: "restaurants/setPageSize",
+      refreshRestaurants: "restaurants/refreshRestaurants"
+    })
+  }
+};
 </script>
