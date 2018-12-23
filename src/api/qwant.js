848f4b46139204d10a5a55d7806ad8c56e1af428
wdev733 <wdev733@gmail.com>
2018-12-23 15:11:38 +0100

    proxy cors header changed

--- src/api/qwant.js
+++ src/api/qwant.js
@@ -1,20 +1,25 @@
 //Code anywhere : https://github.com/Rob--W/cors-anywhere
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+//const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const proxyurl = "https://thingproxy.freeboard.io/fetch/";
+
 const url = `${proxyurl}https://api.qwant.com/api/search/images`;
 
 const qwant = {
-    async image(name, count = 1) {
-        const type = 'images';
-        const uiv = 4
+  async image(name, count = 1) {
+    const type = "images";
+    const uiv = 4;
 
-        try {
-            const response = await fetch(`${url}?q=${name}&count=${count}&t=${type}&uiv=${uiv}`);
-            return await response.json();
-        } catch(err) {
-            console.error('[ERROR] image : ', err);
-            return err;
-        }
-    },
-}
+    try {
+      const response = await fetch(
+        `${url}?q=${name}&count=${count}&t=${type}&uiv=${uiv}`
+      );
+      return await response.json();
+    } catch (err) {
+      console.error("[ERROR] image : ", err);
+      return err;
+    }
+  }
+};
 
 export default qwant;
