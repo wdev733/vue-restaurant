9e7ace208eec340747a802658cd6cea1391bd9da
wdev733 <wdev733@gmail.com>
2018-12-28 12:20:21 +0100

    Add e2e test config

--- tests/e2e/specs/containers/RestaurantsModal/InformationTab.spec-e2e.js
+++ tests/e2e/specs/containers/RestaurantsModal/InformationTab.spec-e2e.js
@@ -1,5 +1,8 @@
-describe('InformationTab', () => {
-    test('It should call restaurant selected getter.', () => {
-        expect(true).toBeTruthy();
-      });
-})
+module.exports = {
+    'navigate to component' : function(browser) {
+        browser.url('http://localhost:8080/')
+        .waitForElementVisible('body', 1000)
+        .assert.title('tp3-resto')
+        .end();
+    }
+}
