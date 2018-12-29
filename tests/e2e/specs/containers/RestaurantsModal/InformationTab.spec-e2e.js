085ae1436b269badb5b18ecc284c95819ff2b066
wdev733 <wdev733@gmail.com>
2018-12-29 19:50:26 +0100

    Create first e2e tests

--- tests/e2e/specs/containers/RestaurantsModal/InformationTab.spec-e2e.js
+++ tests/e2e/specs/containers/RestaurantsModal/InformationTab.spec-e2e.js
@@ -4,5 +4,23 @@ module.exports = {
         .waitForElementVisible('body', 1000)
         .assert.title('tp3-resto')
         .end();
-    }
+    },
+
+    /*
+    'should show Menu Tab': function(browser) {
+        browser
+        .url('http://localhost:8080/')
+        .waitForElementVisible('body', 1000)
+        .assert.cssProperty(".el-dialog__wrapper", 'display', 'none')
+        .elements('css selector', 'td.el-table_1_column_3 > div > button', function(res) {
+            console.log(res.value[0])
+            
+            browser.click(res.value[0].ELEMENT)
+        })
+        .click("table > tbody > tr:nth-child(1) > td.el-table_1_column_3 > div > button", function(result) {
+            console.log(result)
+        })
+        .assert.cssProperty(".el-dialog__wrapper", 'display', 'block')
+        .end()
+    } */
 }
