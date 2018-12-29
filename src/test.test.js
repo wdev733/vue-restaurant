54190d0376422d21cb1d36fa4267fd3cfea86fd5
wdev733 <wdev733@gmail.com>
2018-12-29 19:41:16 +0100

    [TP-10] test when test it's wrong

--- src/test.test.js
+++ src/test.test.js
@@ -2,6 +2,6 @@ const method = () => 2 + 5;
 
 describe("Test CI", () => {
   it("Run Travis CI, good scenario", () => {
-    expect(method()).toEqual(7);
+    expect(method()).toEqual(8);
   });
 });
