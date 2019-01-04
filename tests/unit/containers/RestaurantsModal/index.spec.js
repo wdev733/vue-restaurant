f71447bf0e870f6471409b9d9318a0777aa297ee
wdev733 <wdev733@gmail.com>
2019-01-04 19:27:11 +0100

    Create tests for Restaurant Modal Index

--- tests/unit/containers/RestaurantsModal/index.spec.js
+++ tests/unit/containers/RestaurantsModal/index.spec.js
@@ -2,12 +2,12 @@ import Vuex from 'vuex';
 import Vue from 'vue'
 
 import Element from 'element-ui'
-import InformationTab from '../../../../src/containers/RestaurantModal/InformationsTab.vue';
+import Index from '../../../../src/containers/RestaurantModal/index.vue';
 import {shallowMount, createLocalVue} from '@vue/test-utils';
 import locale from 'element-ui/lib/locale/lang/fr'
 import fakeStore from '../../../mock/store/index';
 
-Vue.use(Element, {locale})
+Vue.use(Element, {locale});
 
 const localVue = createLocalVue();
 
@@ -20,17 +20,17 @@ describe('InformationTab', () => {
   beforeEach(() => {
     store = fakeStore.store;
 
-    wrapper = shallowMount(InformationTab, {
+    wrapper = shallowMount(Index, {
       store: store,
       localVue
     });
   });
 
-  test('It should call restaurant selected getter.', () => {
-    expect(fakeStore.mock.restaurants.getters.restaurantSelected).toHaveBeenCalled();
+  test('openModal should not be null', () => {
+    expect(wrapper.vm._computedWatchers.openModal.value).toBeDefined();
   });
 
-  test('Restaurant image should not be null', () => {
-    expect(wrapper.vm._computedWatchers.image.value).toBe('test');
+  test('restaurantSelected should be called', () => {
+    expect(fakeStore.mock.restaurants.getters.restaurantSelected).toHaveBeenCalled();
   });
 });
