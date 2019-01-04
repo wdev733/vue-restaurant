f6167ad939c49c7f279572f709e9b457fb6a8522
wdev733 <wdev733@gmail.com>
2019-01-04 19:40:21 +0100

    Create ChangePage tests

--- tests/mock/store/restaurants/index.js
+++ tests/mock/store/restaurants/index.js
@@ -3,8 +3,32 @@ const state = {
         page: 1,
         pageSize: 10,
     },
-    count: 0,
-    all: [],
+    count: 2,
+    all: [{
+      name: 'Test',
+      cuisine: 'Cuisine test',
+      address: {
+        coord: [],
+        street: '8 avenue de picardie',
+        zipcode: '06000',
+        building: '2',
+      },
+      borough: '1111',
+      restaurant_id: '1',
+      _id: '111',
+    }, {
+      name: 'Test 2',
+      cuisine: 'Cuisine test 2',
+      address: {
+        coord: [],
+        street: '10 avenue de picardie',
+        zipcode: '06001',
+        building: '2',
+      },
+      borough: '1111',
+      restaurant_id: '1',
+      _id: '112',
+    }],
     selected: {
         name: 'Test',
         cuisine: 'Cuisine test',
@@ -24,18 +48,18 @@ const state = {
         openUpdateModal: false,
     },
     imageCurrentRestaurant: 'test',
-}
+};
 
 const getters = {
     restaurantSelected: jest.fn().mockReturnValue(state.selected),
     coordRestaurantSelected: jest.fn().mockReturnValue(state.selected.address.coord),
     page: jest.fn().mockReturnValue(state.table.page),
     pageSize: jest.fn().mockReturnValue(state.table.pageSize),
-}
+};
 
 
 export default {
     namespaced: true,
-    state, 
+    state,
     getters,
 };
