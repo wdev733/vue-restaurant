8f2946dfa425490161d9f8e3f56664a859526c79
wdev733 <wdev733@gmail.com>
2019-01-09 13:33:53 +0100

    change port to server from 8088 to 8080

--- src/api/restaurants.js
+++ src/api/restaurants.js
@@ -1,59 +1,68 @@
-const url = "http://localhost:8088/api"
+const url = "http://localhost:8080/api";
 
 const restaurants = {
-    async getAll({ page = 1, pageSize = 10, restaurantName }) {
-        if(page) {page = `page=${page}&`}
-        if(pageSize) { pageSize = `pagesize=${pageSize}&` }
-        if(restaurantName) { restaurantName = `name=${restaurantName}&` } 
-        else {restaurantName = '' }
-        
-        try {
-            const response = await fetch(`${url}/restaurants?${page}${pageSize}${restaurantName}`);
-            return await response.json();
-        } catch(err) {
-            console.error('[ERROR] getAll : ', err);
-        }
-    },
-    async getById(id) {
-        try {
-            const response = await fetch(`${url}/restaurants/${id}`);
-            return await response.json();
-        } catch(err) {
-            console.error('[ERROR] getById : ', err);
-        }
-    },
-    async create(formData) {
-        try {
-            const response = await fetch(`${url}/restaurants`, {
-                method: 'POST',
-                body: formData
-            });
-            return await response.json();
-        } catch(err) {
-            console.error('[ERROR] create : ', err);
-        }
-    },
-    async update({id, formData}) {
-        try {
-            const response = await fetch(`${url}/restaurants/${id}`, {
-                method: 'PUT',
-                body: formData
-            });
-            return await response.json();
-        } catch(err) {
-            console.error('[ERROR] update : ', err);
-        }
-    },
-    async delete(id) {
-        try {
-            const response = await fetch(`${url}/restaurants/${id}`, {
-                method: 'DELETE',
-            });
-            return await response.json();
-        } catch(err) {
-            console.error('[ERROR] delete : ', err);
-        }
+  async getAll({ page = 1, pageSize = 10, restaurantName }) {
+    if (page) {
+      page = `page=${page}&`;
     }
-}
+    if (pageSize) {
+      pageSize = `pagesize=${pageSize}&`;
+    }
+    if (restaurantName) {
+      restaurantName = `name=${restaurantName}&`;
+    } else {
+      restaurantName = "";
+    }
+
+    try {
+      const response = await fetch(
+        `${url}/restaurants?${page}${pageSize}${restaurantName}`
+      );
+      return await response.json();
+    } catch (err) {
+      console.error("[ERROR] getAll : ", err);
+    }
+  },
+  async getById(id) {
+    try {
+      const response = await fetch(`${url}/restaurants/${id}`);
+      return await response.json();
+    } catch (err) {
+      console.error("[ERROR] getById : ", err);
+    }
+  },
+  async create(formData) {
+    try {
+      const response = await fetch(`${url}/restaurants`, {
+        method: "POST",
+        body: formData
+      });
+      return await response.json();
+    } catch (err) {
+      console.error("[ERROR] create : ", err);
+    }
+  },
+  async update({ id, formData }) {
+    try {
+      const response = await fetch(`${url}/restaurants/${id}`, {
+        method: "PUT",
+        body: formData
+      });
+      return await response.json();
+    } catch (err) {
+      console.error("[ERROR] update : ", err);
+    }
+  },
+  async delete(id) {
+    try {
+      const response = await fetch(`${url}/restaurants/${id}`, {
+        method: "DELETE"
+      });
+      return await response.json();
+    } catch (err) {
+      console.error("[ERROR] delete : ", err);
+    }
+  }
+};
 
 export default restaurants;
